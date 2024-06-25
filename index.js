d869833cf9fc3d780ebc9c549837e1e8ba819c6b
LindaTa <lindata@maneandmoon.com>
2024-06-25 12:56:14 -0500

    control flow lab comp

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+
+  if(distance > 2500){
+    return ('No can do.') 
+  }
+
+  else if(distance < 400){
+    return ('This one is on me!')
+  }
+
+  else if(distance >= 400 && distance < 2000) {
+    return ('That will be twenty bucks.')
+  }
+
+  else if(distance > 2000){
+    return ('I will gladly take your thirty bucks.')
+  } 
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(location){
   // Write your code here!
+return location ==='NYC' ? "Ok, sounds good." : "No go."
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(value){
   // Write your code here!
+ // if(value === 'generous'){
+  //  return 'Thank you so much.'
+  
+switch(value){
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+     default:
+      return 'Bye.'   
+  }
+
 }
